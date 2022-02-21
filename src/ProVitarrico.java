b4ef92d6d513f0cd4424d10d432a39bf9ecc16e1
Samuel Aroca <samuel.arocad@unac.edu.co>
2022-02-21 15:44:49 -0500

    System

--- src/ProVitarrico.java
+++ src/ProVitarrico.java
@@ -1,6 +1,6 @@
 public class ProVitarrico {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("A");
     }//git config --global user.email "you@example.com"
     //git config --global user.name "Your Name"
 }
