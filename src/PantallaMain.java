f70588a3c4d8d7bd1ba7f97b431d4469e854db5e
SamuelAroca <samuel.arocad@unac.edu.co>
2022-03-25 14:27:24 -0500

    Links

--- src/PantallaMain.java
+++ src/PantallaMain.java
@@ -2,15 +2,24 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class PantallaMain {
     protected JPanel panelMain;
     private JButton buttonRegistro;
     private JButton buttonLogin;
     private JLabel labelLogo;
+    private JLabel labelWhapp;
+    private JLabel labelVideo;
+    private JLabel labelTectoVideo;
 
 
     public PantallaMain() {
+
 
 
 
@@ -20,6 +29,38 @@ public class PantallaMain {
 
             }
         });
+        labelWhapp.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                enlace("https://www.whatsapp.com/?lang=es");
+            }
+        });
+        labelVideo.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                enlacePubli("https://youtu.be/WWbjMbjcvt0");
+            }
+        });
+    }
+
+    public void enlace(String enlaceAAceder) {
+        Desktop enlace = Desktop.getDesktop();
+        try {
+            enlace.browse(new URI(enlaceAAceder));
+
+        }catch (IOException | URISyntaxException e) {
+            e.getMessage();
+        }
+    }
+
+    public void enlacePubli(String enlaceAAceder) {
+        Desktop enlace = Desktop.getDesktop();
+        try {
+            enlace.browse(new URI(enlaceAAceder));
+
+        }catch (IOException | URISyntaxException e) {
+            e.getMessage();
+        }
     }
 
 
--- src/ProVitarrico.java
+++ src/ProVitarrico.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 
 public class ProVitarrico {
+
+
     public static void main(String[] args) {
         JFrame frameMain = new JFrame("Proyecto Integrador");
         frameMain.setContentPane(new PantallaMain().panelMain);
-        frameMain.setSize(950,500);
+        frameMain.setSize(650,500);
         frameMain.setLocationRelativeTo(null);
         frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frameMain.setVisible(true);
